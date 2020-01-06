1b7dde1decbec151c3c18a0e3809fa340bd76bb9
floppyvlaDISK <tarasenkokansasvlad@gmail.com>
2020-01-06 15:05:42 +0200

    Register empty service worker

--- client.js
+++ client.js
@@ -1,1 +1,26 @@
-renderGallery(document.body);
+registerServiceWorker()
+  .then(renderPage)
+  .catch(renderPage);
+
+function registerServiceWorker() {
+  return new Promise((resolve, reject) => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('./sw.js')
+        .then(() => {
+          console.log('sw registration completed');
+          resolve();
+        })
+        .catch(err => {
+          console.warn('sw registration failed with', err);
+          reject();
+        });
+    } else {
+      console.warn('sw is unsupported');
+      reject();
+    }
+  });
+}
+
+function renderPage() {
+  renderGallery(document.body);
+}
