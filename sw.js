904491ac145998d8b079714b643ed6776690e8ba
floppyvlaDISK <tarasenkokansasvlad@gmail.com>
2020-01-06 15:59:55 +0200

    Fetch assets and then cache

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+const CACHE_VER = 'v1';
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
+    caches.open(CACHE_VER).then(cache => {
       return cache.addAll([
         './index.html',
         './images-list.js',
@@ -9,6 +11,19 @@ self.addEventListener('install', (event) => {
         './gallery/myLittleVader.jpg',
         './gallery/snowTroopers.jpg',
       ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(matchResponse => {
+      return matchResponse || fetch(event.request).then(fetchResponse => {
+        return caches.open(CACHE_VER).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
     })
   );
 });
